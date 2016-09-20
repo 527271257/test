744e57b2f0619a1c2e2c808172a5689755a68ac1
527271257 <527271257@qq.com>
2016-09-20 08:27:24 -0400

    edit test-build.sh

--- test-bulid/test-build.sh
+++ test-bulid/test-build.sh
@@ -1,4 +1,6 @@
-RKSPACE}/src
+#!/bin/sh
+cd ${WORKSPACE}/src
+
 # WORKSPACE环境变量为/var/jenkins_home/jobs/CITEST/workspace
 docker build -t 192.168.5.34:80/python-redis-demo:${BUILD_NUMBER} .
 #根据BUILD_NUMBER的变化创建一个新的容器镜像。
@@ -13,7 +15,6 @@ sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.
 #将占位符赋值为5000，替换为5000加上变量BUILD_NUMBER的值。
 chmod777 ./rancher-compose
 
-./rancher-compose --access-key EF156F5D22D46A81D9B0 --secret-key QMGwZjWJqNaV8F7aiETmzWRo6zJnarBBKzSzUJoh -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key 339B16584497A27010F5  --secret-key BL1Zpa7gyHx3v2BUtmGMoyXacJkqPWtvgBfhb7vA -p python-redis-demo-build${BUILD_NUMBER} up -d
 #运行Rancher-compose命令创建一个Stack，名称为python-redis-demo-build加变量{BUILD_NUMBER}的值。添加我们刚刚复制的access key和secret key。
-
 
